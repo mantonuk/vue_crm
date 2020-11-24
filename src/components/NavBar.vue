55b92c109f2a54ffeb812973949fbd2b93e6470a
Maxim Antonuk <maxim.antonuk@gmail.com>
2020-11-25 00:39:52 +0200

    Date + logout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,7 +5,7 @@
         <a href="#" @click.prevent="$emit('click');">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{ date }}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -28,7 +28,7 @@
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -42,13 +42,33 @@
 <script>
 export default {
   name: "NavBar",
+  data : () => ({
+    date: new Date(),
+    interval: null,
+    dropdown: null
+  }),
+  methods : {
+    logout() {
+      console.log('logout');
+      this.$router.push('/login');
+    }
+  },
   mounted() {
-    window.M.Dropdown.init(
+    this.interval = setInterval(() => {
+      this.date = new Date()
+    }, 1000);
+    this.dropdown = window.M.Dropdown.init(
       this.$refs.dropdown,
       {
         constrainWidth: false
       }
     );
+  }, 
+  beforeDestroy() {
+    clearInterval(this.interval);
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy();
+    }
   }
 };
 </script>
